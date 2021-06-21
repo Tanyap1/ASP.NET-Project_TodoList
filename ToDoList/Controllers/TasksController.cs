39bf8de59bd68d3a59a96e897bcb49c6094e48bc
github-Tanyap1 <github-Tanya.Pachkalov@MyGeorgian.ca>
2021-06-21 11:46:06 -0400

    todolist.task

--- ToDoList/Controllers/TasksController.cs
+++ ToDoList/Controllers/TasksController.cs
@@ -1,10 +1,13 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ToDoList.Data;
 using ToDoList.Models;
+using Task = ToDoList.Models.Task;
 
 namespace ToDoList.Controllers
 {
