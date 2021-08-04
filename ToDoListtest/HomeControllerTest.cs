017c1da351a5c15688b2f93a049f397c90059530
github-Tanyap1 <github-Tanya.Pachkalov@MyGeorgian.ca>
2021-08-04 12:29:26 -0400

    test

---
+++ ToDoListtest/HomeControllerTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoListtest
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void IndexReturnsResult()
+        {
+            var controller = new HomeController(null);
+            var result = controller.Index();
+            Assert.IsNotNull(result);
+        }
+            
+[TestMethod]
+public  void PrivacyLoadsPrivacyView()
+        {
+            var controller = new HomeController(null);
+            var result = (ViewResult)controller.Privacy();
+
+        }
+
+    }
+}
