b8ff52349f273050276705618fadca1856226249
Tanyap1 <78969794+Tanyap1@users.noreply.github.com>
2021-08-11 11:15:03 -0400

    Update TasksControllerTest.cs

--- ToDoListtest/TasksControllerTest.cs
+++ ToDoListtest/TasksControllerTest.cs
@@ -1,66 +1,187 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ToDoList.Controllers;
-using ToDoList.Data;
-using ToDoList.Models;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Data;
+using ToDoList.Models;
+using Task = ToDoList.Models.Task;
 
-namespace ToDoListtest
+namespace ToDoList.Controllers
 {
-    [TestClass]
-    public class TasksControllerTest
+    [Authorize (Roles = "Administrator")]
+
+    public class TasksController : Controller
     {
-        private ApplicationDbContext _context;
-        List<ToDoList.Models.Task> tasks = new List<ToDoList.Models.Task>();
-        TasksController controller;
+        private readonly ApplicationDbContext _context;
+        private readonly object stream;
 
-
-        [TestInitialize]
-
-        public void TestInitialize()
+        public TasksController(ApplicationDbContext context)
         {
-            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-                .UseInMemoryDatabase(Guid.NewGuid().ToString())
-                .Options;
-            _context = new ApplicationDbContext(options);
+            _context = context;
+        }
 
-            var list = new List { Id = 100, Task = "Test Category" };
-            _ = _context.Lists.Add(list);
-            _context.SaveChanges();
+        // GET: Tasks
 
-            tasks.Add(new ToDoList.Models.Task { Id = 101, Name = "name1", ListId = 100 });
-            tasks.Add(new ToDoList.Models.Task { Id = 102, Name = "name2", ListId = 101 });
-            tasks.Add(new ToDoList.Models.Task { Id = 103, Name = "name3", ListId = 102 });
+#pragma warning disable CS0114 // Member hides inherited member; missing override keyword
+        private IActionResult View(object p)
+#pragma warning restore CS0114 // Member hides inherited member; missing override keyword
+        {
+            throw new NotImplementedException();
+        }
 
-            foreach (var p in tasks)
-
+        // GET: Tasks/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
             {
-                _context.Tasks.Add(p);
+                return NotFound();
             }
-            _context.SaveChanges();
-            controller = new TasksController(_context);
-        }
-        [TestMethod]
-        public void IndexViewLoads()
-        {
-            var result = controller.Index();
-            var viewResult =  result as ViewResult;
-            Assert.AreEqual("Index", viewResult.ViewName);
-        }
-        [TestMethod]
-        public void IndexReturnsTaskData()
 
+            var task = await _context.Tasks
+                .Include(t => t.List)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return View(task);
+        }
+
+        // GET: Tasks/Create
+        public IActionResult Create()
         {
-            var result = controller.Index();
-            var viewResult =
-                result as ViewResult;
-            var model = (List<ToDoList.Models.Task>)viewResult.Model;
-            CollectionAssert.AreEqual(tasks, model);
+            ViewData["ListId"] = new SelectList(_context.Lists, "Id", "Task");
+            return View();
+        }
+
+        public object Index()
+        {
+            throw new NotImplementedException();
+        }
+
+        // POST: Tasks/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Importance,ListId")] Task task, IformFile Image)
+        {
+            if (ModelState.IsValid)
+            {
+                //img
+                if (Image != null)
+                { 
+                    var filePath = Path.GetTempFileName();
+                    var fileName = Guid.NewGuid() + "-" + Image.FileName;
+                    var uploadPath = System.IO.Directory.GetCurrentDirectory() + "\\wwwroot\\img\\task\\" + fileName;
+
+                    //strem
+                    using (var strem = new FileStream(uploadPath, FileMode.Create))
+                    {
+                        await Image.CopyToAsync(stream);
+                    }
+                    task.Image = fileName;
+                }
+                _context.Add(task);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ListId"] = new SelectList(_context.Lists, "Id", "Task", task.ListId);
+            return View(task);
+        }
+
+        // GET: Tasks/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var task = await _context.Tasks.FindAsync(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+            ViewData["ListId"] = new SelectList(_context.Lists, "Id", "Task", task.ListId);
+            return View(task);
+        }
+
+        // POST: Tasks/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Importance,Image,ListId,TaskCategoryId")] Task task)
+        {
+            if (id != task.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(task);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TaskExists(task.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ListId"] = new SelectList(_context.Lists, "Id", "Task", task.ListId);
+            return View(task);
+        }
+
+        // GET: Tasks/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var task = await _context.Tasks
+                .Include(t => t.List)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return View(task);
+        }
+
+        // POST: Tasks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+            _context.Tasks.Remove(task);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TaskExists(int id)
+        {
+            return _context.Tasks.Any(e => e.Id == id);
         }
     }
 }
