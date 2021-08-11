73045072504552d238a700778b8ff1c92f301d1d
github-Tanyap1 <github-Tanya.Pachkalov@MyGeorgian.ca>
2021-08-11 13:40:11 -0400

    api

---
+++ ToDoList/API/ValuesController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ToDoList.API
+{
+    //Endpoint will be http://localhost:44368/api/Values
+    [Route("api/[controller]")]
+
+    //marks this as an API controller
+    [ApiController]
+
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public string Post([FromBody] string value)
+        {
+            return "Deleted successfully";
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            
+        }
+    }
+}
